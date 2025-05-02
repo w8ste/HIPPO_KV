e718c7db733a8d368d9225ed8b187c4d455efcd6
Hannes <hannes10042004@icloud.com>
2025-05-02 22:15:18 +0200

    put

--- src/hippo_kv.cpp
+++ src/hippo_kv.cpp
@@ -1,8 +1,8 @@
 #include "hippo_kv.hpp"
 #include <iostream>
-#include <cstdint>
 
-enum op_code {PUT = (uint8_t)0x01, PUT_SHALLOW = (uint8_t)0x02, GET = (uint8_t)0x03};
+enum op_code {PUT = (uint8_t)0x01, PUT_SHALLOW = (uint8_t)0x02, DEL = (uint8_t)0x03};
+
 
 HIPPOKV::HIPPOKV(const std::string& file_name) : file_name(file_name) {
   file.open(file_name, std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
@@ -13,6 +13,10 @@ HIPPOKV::HIPPOKV(const std::string& file_name) : file_name(file_name) {
 
   replay_log();
 
+  for (auto i : db_map) 
+    std::cout << i.first << ": " << i.second
+              << std::endl;
+
 }
 
 HIPPOKV::~HIPPOKV() {
@@ -22,6 +26,12 @@ HIPPOKV::~HIPPOKV() {
 }
 
 void HIPPOKV::hippo_put(const std::string& key, const std::string& value) {
+
+  if (!file.is_open()) {
+    std::cerr << "File is not open!\n";
+    return;
+}
+
   op_code op = PUT;
   uint32_t key_size = key.size();
   uint32_t value_size = value.size();
@@ -31,8 +41,71 @@ void HIPPOKV::hippo_put(const std::string& key, const std::string& value) {
   file.write(key.c_str(), key_size);
   file.write(reinterpret_cast<const char*>(&value_size), sizeof(value_size));
   file.write(value.c_str(), value_size);
-
+  file.flush();
   db_map[key] = value;
+
+    for (auto i : db_map) 
+    std::cout << i.first << ": " << i.second
+              << std::endl;
+
 }
 
-void HIPPOKV::replay_log() {}
+void HIPPOKV::hippo_delete(const std::string& key) {
+  op_code op = DEL;
+  const std::string& value = "\0";
+
+  hippo_write_to_db(op, key, value);
+
+  db_map.erase(key);
+}
+
+void HIPPOKV::hippo_write_to_db(uint8_t op, const std::string &key,
+                                const std::string &value) {
+  uint32_t key_size = key.size();
+  uint32_t value_size = value.size();
+
+  file.write(reinterpret_cast<const char*>(&op), sizeof(op));
+
+  file.write(reinterpret_cast<const char*>(&key_size), sizeof(key_size));
+  file.write(key.c_str(), key_size);
+
+  file.write(reinterpret_cast <const char*>(&value_size), sizeof(value_size));
+  file.write(value.c_str(), value_size);
+}
+
+bool HIPPOKV::hippo_get(const std::string& key, std::string& value) {
+  auto it = db_map.find(key);
+
+  if(it != db_map.end()) {
+    value = it->second;
+    return true;
+  }
+  return false;
+}
+
+void HIPPOKV::replay_log() {
+
+  while(file) {
+
+    uint8_t op;
+    uint32_t key_size;
+    uint32_t value_size;
+
+    file.read(reinterpret_cast<char*>(&op), sizeof(op));
+    file.read(reinterpret_cast<char*>(&key_size), sizeof(key_size));
+    file.read(reinterpret_cast<char*>(&value_size), sizeof(value_size));
+
+    std::string key(key_size, '\0');
+    std::string value(value_size, '\0');
+          
+    file.read(&key[0], key_size);
+    file.read(&value[0], value_size);
+
+    if (op == PUT) {
+      db_map[key] = value;
+    }
+    else if (op == DEL) {
+      db_map.erase(key);
+    }
+  }
+}
